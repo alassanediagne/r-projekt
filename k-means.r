b7875a06cf770c9c78d79926c34f96490e980e7e
alassanediagne <alassanediagne@outlook.de>
2024-07-08 20:08:45 +0200

    Algorithmus 9.10. für k means implementiert

--- k-means.r
+++ k-means.r
@@ -19,72 +19,50 @@ update_C <- function(x, m){
     current_mean <- matrix(m[,k], nrow = d, ncol = n)
     dist_to_mean <- x - current_mean #subrahiere den aktuellen mean
     dist_to_mean <- apply(dist_to_mean, 2, \(x) sum(x^2)) # bestimme alle quadrierten eukl. Normen
-    to_update <- dist_to_mean < current_min_dist
-    current_arg_mins[to_update] <- rep(k, times=sum(to_update))
-    current_min_dist[to_update] <- dist_to_mean[to_update]
+    to_update <- dist_to_mean < current_min_dist # bestimme die Indizes zu denen eine kleinere Distanz gefunden wurde
+    current_arg_mins[to_update] <- rep(k, times=sum(to_update)) # update argmins
+    current_min_dist[to_update] <- dist_to_mean[to_update] # update minimale Distanzen
   return(current_arg_mins)
   }
 }
 
-x <- matrix(1:28,nrow=7)
-x
-m <- matrix(1:14, nrow = 7)
-update_C(x,m)
 
-update_m <- function(K, x, d, C){
-  n <- length(x)
-  m <- rep(0, times=K)
-  for(k in 1:K){
-    mask <- C == k
-    mask <- mask %>% rep(times=d) %>% matrix(ncol=n, nrow=d)
-    
-    x_relevant <- x %>% matrix(ncol=n, nrow=d)
-    x_relevant <- c(x_relevant[mask])
-    
-    mk <- vec_sum(x_relevant, d) / sum(mask)
-    m[k] <- mk
+
+update_m <- function(x,C,num_cluster){
+  # x dxn Matrix mit Daten, C: Vektor mit aktuellen argmins, num_cluster: Anzahl der Cluster
+  n <- ncol(x)
+  d <- nrow(x)
+  m <- matrix(nrow=d, ncol=num_cluster)
+  
+  for(k in 1:num_cluster){
+    mask <- C == k # prüfe für welche der x_i ob k aktuelles argmin ist
+    mask <- mask %>% rep(each=d) %>% matrix(ncol=n, nrow=d)
+    x_relevant <- x[mask] %>% matrix(nrow=d) # maskiere alle x_i deren aktuellen argmin nicht k ist
+    m[,k] <- apply(x_relevant, 1, mean) # update mean
   }
   return(m)
 }
 
 
-k_means <- function(x,num_cluster, m0 = NULL, max_iter = 100, tol = 1e-5){
-  # x: dxn - Matrix mit Daten, k Anzahl der Cluster, m0 Anfangswerte
-  prep <- prepare_data(x)
-  x <- prep$data
-  d <- prep$dimension
-  n_iter <- 0L
+
+k_means <- function(x,num_cluster, m0, max_iter = 10, tol = 1e-3){
+  # x: dxn - Matrix mit Daten, num_cluster: Anzahl der Cluster, m0: Anfangswerte
+  n_iter <- 0L # zählt Iterationen
   m <- m0
+  
   while(n_iter < max_iter){
-    m_old <- m
-    n_iter <- m_iter + 1L
-    C <- update_C(x,m,d)
-    m <- update_m(num_cluster, x, d, C)
-    if(vec_norm(m_old-m)<tol){
-      return(m)
+    m_old <- m # speichere m zum vergleichen
+    current_arg_mins <- update_C(x,m) # update argmins
+    m <- update_m(x,current_arg_mins,num_cluster) # update means
+    if(norm(m-m_old)<tol){
+      # prüfe konvergenz
+      return(list(means=m, msg="Methode knovergiert"))
     }
+    n_iter <- n_iter + 1L
   }
+  return(list(means=m, msg="Maximale Anzahl der Iterationen erreicht"))
 }
 
-x <- list(c(1,2,3),c(3,2,4),c(1,9,8))
-prep <- prepare_data(x)
-x <- prep$data
-d <- prep$dimension
-x<-1:3
-
-c(1,2,3,4) < c(3,2,4,1)
-
-x
-x[c(T,F,T)] <- rep(2, times=sum(c(T,F,T)))
-x
- v <- c(1,2,3,5,6)
- matrix(v, ncol=5,nrow=length(v))
- 
-m <- list(c(4,5,6),c(1,2,3))
-C <- update_C(x,m,3)
-update_m(2,x,3,C)
-m <- 3:5
-n <- 1:3
-seq_along(1:5)
-sum(c(T,F,T,F))
-matrix(1:12, ncol=3)
+x <- matrix(c(1:28,1:7),nrow=7)
+m <- matrix(1:14, nrow = 7)
+k_means(x,2,m)
