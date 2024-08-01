0d10869ef2b908dedd45ffce5fd003874b19826e
RobinKlotzbuecher <robin.klotzbuecher@gmail.com>
2024-08-01 17:44:28 +0200

    Changed some names

--- R/spectral-clustering.R
+++ R/spectral-clustering.R
@@ -6,47 +6,47 @@ h <- 10
 spectral_clustering <- function(data, f_kernel, k){
     
     n <- ncol(data) 
-    W <- matrix(0, nrow=n, ncol=n) #Gewichtsmatrix initialisieren
+    affinity_matrix <- matrix(0, nrow=n, ncol=n) #Gewichtsmatrix initialisieren
    
     i <- 1
     j <- 1
     for (i in 1:n){
         for (j in 1:n)
-            {W[i,j]=f_kernel(data[,i], data[,j])} #Gewichte zwischen Daten bestimmen
+            {affinity_matrix[i,j]=f_kernel(data[,i], data[,j])} #Gewichte zwischen Daten bestimmen
     }
     
-    D <- matrix(0,nrow=n,ncol=n) #Diagonalmatrix initialisieren
+    diagonal_matrix <- matrix(0,nrow=n,ncol=n) #Diagonalmatrix initialisieren
     i <- 1
     j <- 1
     for(i in 1:n){
         a <- 0
         for (j in 1:n){
             a <- a+f_kernel(data[,i],data[,j]) #Werte der Diagonalmatrix bestimmen
-            D[i,i]=a
+            diagonal_matrix[i,i]=a
         }
     }
     
-    L <- D-W #Laplacematrix berechnen
+    laplace_matrix <- diagonal_matrix-affinity_matrix #Laplacematrix berechnen
     
     "%^%" <- function(x, q) #Funktion zur Berechnung von negativen, nicht ganzen Potenzen von Matrizen definieren
         {with(eigen(x), vectors %*% (values^q * t(vectors)))}
    
     
-    eigen_L <- eigen((D%^%(-0.5))*L*(D%^%(-0.5))) #Eigenwerte und -vektoren der Laplacematrix bestimmen
+    eigen_L <- eigen((diagonal_matrix%^%(-0.5))*laplace_matrix*(diagonal_matrix%^%(-0.5))) #Eigenwerte und -vektoren der Laplacematrix bestimmen
     eigenvectors_L <- eigen_L$vectors[,order(eigen_L$values)] #Eigenvektoren von L nach aufsteigender Größe der Eigenwerte sortieren
     eigenvectors_logical_L <- logical(n) #Wahrheitsvektor für genormte Eigenvektoren initialisieren
     
     for (i in 1:n){
         if (norm(eigenvectors_L[,i], type="2")==1) #Eigenvektoren mit Norm=1 bestimmen
-            eigenlog[i]=TRUE
+            eigenvectors_logical_L[i]=TRUE
         }
-    eigenvectors_norm_L <- e$vectors[,eigenlog] #Matrix mit Eigenvektoren der Länge 1 als Spalten erstellen       
+    eigenvectors_norm_L <- eigen_L$vectors[,eigenvectors_logical_L] #Matrix mit Eigenvektoren der Länge 1 als Spalten erstellen       
     
     spectral_projections <- matrix(0,nrow=n, ncol=n) #Matrix mit optimalen Projektionen initialisieren
     
     i <- 1
-    for (i in 1:length(eigenlog)) #Matrix mit optimalen Projektionen erstellen
-        {spectral_projections[i,] = (n^(1/2))*(D%^%(-0.5))%*%eigenvectors_norm_L[,i]}        
+    for (i in 1:length(eigenvectors_logical_L)) #Matrix mit optimalen Projektionen erstellen
+        {spectral_projections[i,] = (n^(1/2))*(diagonal_matrix%^%(-0.5))%*%eigenvectors_norm_L[,i]}        
     
     
     k_spectral_projections <- spectral_projections[2:(k+1),] #optimale k-dimensionale Projektionen bestimmen
