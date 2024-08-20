7e45b259d66b576e463b1444e4ee8e9cd22a1729
RobinKlotzbuecher <robin.klotzbuecher@gmail.com>
2024-08-20 20:19:54 +0200

    update spectral clustering (added plotting)

--- R/spectral-clustering.R
+++ R/spectral-clustering.R
@@ -1,8 +1,8 @@
 
 gausskernel <- function(x,y) {exp(-h*(norm(x-y, type="2"))^2)} #Gausskern definieren
-h <- 100
+h <- 50
 
-spectral_clustering <- function(data, f_kernel, k){
+spectral_clustering <- function(data, f_kernel, k,clusters){
     
     n <- nrow(data) 
     affinity_matrix <- matrix(0, nrow=n, ncol=n) #Gewichtsmatrix initialisieren
@@ -23,30 +23,28 @@ spectral_clustering <- function(data, f_kernel, k){
         }
     }
     
+    normalized_diagonal_matrix <- diagonal_matrix
+    for(i in 1:n){
+        normalized_diagonal_matrix[i,i]=(normalized_diagonal_matrix[i,i])^(-1/2)
+    }
+    
+    
     laplace_matrix <- diagonal_matrix-affinity_matrix #Laplacematrix berechnen
     
-    "%^%" <- function(x, q) #Funktion zur Berechnung von negativen, nicht ganzen Potenzen von Matrizen definieren
-        {with(eigen(x), vectors %*% (values^q * t(vectors)))}
    
     
-    eigen_L <- eigen((diagonal_matrix%^%(-0.5))%*%laplace_matrix%*%(diagonal_matrix%^%(-0.5))) #Eigenwerte und -vektoren der Laplacematrix bestimmen
+    eigen_L <- eigen(normalized_diagonal_matrix%*%laplace_matrix%*%normalized_diagonal_matrix) #Eigenwerte und -vektoren der Laplacematrix bestimmen
     eigenvectors_L <- eigen_L$vectors[,order(eigen_L$values)] #Eigenvektoren von L nach aufsteigender Größe der Eigenwerte sortieren
-    eigenvectors_logical_L <- logical(n) #Wahrheitsvektor für genormte Eigenvektoren initialisieren
     
-    for (i in 1:n){
-        if (norm(eigenvectors_L[,i], type="2")==1) #Eigenvektoren mit Norm=1 bestimmen
-            eigenvectors_logical_L[i]=TRUE
-        }
-    eigenvectors_norm_L <- eigen_L$vectors[,eigenvectors_logical_L] #Matrix mit Eigenvektoren der Länge 1 als Spalten erstellen       
     
-    spectral_projections <- matrix(0,  nrow=n, ncol=ncol(eigenvectors_norm_L)) #Matrix mit optimalen Projektionen initialisieren
+    spectral_projections <- matrix(0,  nrow=n, ncol=n) #Matrix mit optimalen Projektionen initialisieren
     
-    for (i in 1:ncol(eigenvectors_norm_L)) #Matrix mit optimalen Projektionen erstellen
-        {spectral_projections[,i] = (n^(1/2))*(diagonal_matrix%^%(-0.5))%*%eigenvectors_norm_L[,i]}        
+    for (i in 1:n) #Matrix mit optimalen Projektionen erstellen
+        {spectral_projections[,i] = (n^(1/2))*normalized_diagonal_matrix%*%eigenvectors_L[,i]}        
     
     
     k_spectral_projections <- spectral_projections[,2:(k+1)] #optimale k-dimensionale Projektionen bestimmen
-    return(k_spectral_projections)
+    
+    spectral_clustering_result <- kmeans(k_spectral_projections, centers = clusters)$cluster
+    plot(data, col=spectral_clustering_result, pch=19)
 }
-
-#anschließend z.B. k-means clustering
