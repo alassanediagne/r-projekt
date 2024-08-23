6b0bf78caee72158c07ad4e15430c55c39673e9e
RobinKlotzbuecher <robin.klotzbuecher@gmail.com>
2024-08-23 13:57:49 +0200

    added tests spectral-clustering

---
+++ tests/testthat/test-spectral-clustering.R
@@ -0,0 +1,46 @@
+library(testthat)
+
+data <- rbind(
+  c(1,0),
+  c(1.1,0),
+  c(1.3,0),
+  c(1.4,0),
+  c(1,0.2),
+  c(1.1,0.2),
+  c(1.3,0.4),
+  c(1.4,-0.2),
+  c(2,0),
+  c(2.1,0),
+  c(2,0.4),
+  c(2.1,0.6),
+  c(2,0.3),
+  c(2.1,0.1),
+  c(2.3,0),
+  c(-3,2),
+  c(1.6, 3)
+)
+
+test_that("spectral_clustering does not throw errors", {
+  expect_no_error(spectral_clustering(data, 4))
+  expect_no_warning(spectral_clustering(data, 4))
+})
+
+test_that("spectral_clustering generates the correct number of projections", {
+  result <- spectral_clustering(data, 4)
+  expect_equal(length(result[,1]), 17)
+})
+
+test_that("correct number of clusters generated", {
+  expect_equal(nrow(k_means_spectral_clustering(data,4)$means),4)
+})
+
+
+test_that("k_means_spectral_clustering assigns seperate clusters for outlier points",{
+  labels <- k_means_spectral_clustering(data, 4)$labels
+  for(i in 1:16){
+  expect_true(labels[17]!=labels[i])
+  }
+  for(i in 1:15){
+    expect_true(labels[16]!=labels[i])
+  }
+})
