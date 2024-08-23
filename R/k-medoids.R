5bb401571f7c4ea779f25e4d93fdaba9dc6df6d7
alassanediagne <alassanediagne@outlook.de>
2024-08-23 18:14:24 +0200

    Update k-medoids.R

--- R/k-medoids.R
+++ R/k-medoids.R
@@ -1,36 +1,34 @@
-compute_distances <- function(data) {
-  stats::dist(data, method = "euclidean") %>% as.matrix()
+update_labels <- function(data, medoids) {
+  # updates labels of data
+  distances <- as.matrix(dist(data, method = "euclidean"))[, medoids]
+  labels <- apply(distances, 1, which.min)
+  return(labels)
 }
 
-update_C <- function(distance_matrix, medoids) {
-  apply(distance_matrix, 1, function(x) {
-    which.min(x[medoids])
-  })
-}
 
-update_medoids <- function(data, C, num_cluster) {
-  medoids <- numeric(num_cluster)
-  for (k in 1:num_cluster) {
-    cluster_points <- data[C == k, , drop = FALSE]
-    if (nrow(cluster_points) > 0) {
-      medoid <- apply(cluster_points, 1, function(point) {
-        sum(rowSums((cluster_points - point) ^ 2))
-      })
-      medoids[k] <- which.min(medoid)
+update_medoids <- function(data, num_cluster, labels, medoids) {
+  # updates medoids
+  new_medoids <- rep(0, times=num_cluster)
+  for (i in 1:num_cluster) {
+    cluster_idx <- which(labels == i)
+    if (length(cluster_idx) > 0) {
+      cluster_points <- data[cluster_idx, ]
+      distances <- as.matrix(dist(cluster_points))
+      distances <- colSums(distances)
+      new_medoids[i] <- cluster_idx[which.min(distances)]
     } else {
-      # Handle the case where a cluster has no points assigned to it
-      medoids[k] <- sample(1:nrow(data), 1)
+      # do not change medoid if the cluster is empty
+      new_medoids[i] <- medoids[i]
     }
   }
-  return(medoids)
+  return(new_medoids)
 }
 
 #' @title k-medoids clustering
 #' @name k_medoids
 #' @param data data matrix. Every row contains a point
 #' @param num_cluster int. number of clusters desired
-#' @param max_iter (optinal) int. sets maximum number of iterations. Default: 50
-#' @param tol (optinal) float. tolerance to conclude convergence Default: 1e-8
+#' @param max_iter (optinal) int. sets maximum number of iterations. Default: 100
 #'
 #' @return list containing logical indicating whether the algorithm converged, number of iterations, cluster medoids
 #' @export
@@ -39,34 +37,33 @@ update_medoids <- function(data, C, num_cluster) {
 #' data <- gen_clusters(50, matrix(c(0,1,2,1,0,1,2,0),ncol=2), 0.2)
 #' k_medoids(data,4)
 
-k_medoids <- function(data, num_cluster, max_iter = 100L, tol = 1e-3) {
 
-  distance_matrix <- compute_distances(data)
-  medoids <- sample(1:nrow(data), num_cluster)
+k_medoids <- function(data, num_cluster, max_iter = 100L) {
 
-  n_iter <- 0L
-  converged <- FALSE
-
-  while (n_iter <= max_iter && !converged) {
-    C <- update_C(distance_matrix, medoids)
-    new_medoids <- update_medoids(data, C, num_cluster)
-
-    if (norm(data[medoids,]-data[new_medoids,], type="2")<tol) {
-      converged <- TRUE
-    } else {
-      medoids <- new_medoids
-    }
-
-    n_iter <- n_iter + 1L
+  if(num_cluster > nrow(data)){
+    stop(sprintf("tried to generate %i clusters with %i data points", num_cluster, nrow(data)))
   }
 
+  n <- nrow(data)
+  converged <- FALSE
+  n_iter <- 0L
 
-  return(list(
-    converged = converged,
-    n_iter = n_iter,
-    medoids = data[medoids,],
-    labels = C
-  ))
+  medoids <- sample(1:n, num_cluster)
+
+  for (i in 1:max_iter) {
+    labels <- update_labels(data, medoids)
+    new_medoids <- update_medoids(data, num_cluster, labels,medoids)
+    n_iter <- n_iter+1
+    if (all(medoids == new_medoids)) {
+      # converged if medoids do not change
+      converged <- TRUE
+      break
+    }
+
+    medoids <- new_medoids
+  }
+
+  return(list(converged = converged, n_iter = n_iter, medoids = data[medoids,], labels = labels))
 }
 
 
